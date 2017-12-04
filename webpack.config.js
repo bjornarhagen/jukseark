f21b92bb3627d9863db29532ec3a96bc5142572a
Bear Hagen <b@datahjelpen.no>
2017-12-04 12:00:21 +0100

    Added linux paper

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,12 @@ module.exports = {
 	},
 	plugins: [
 		new CopyWebpackPlugin([
-			{ from: './src/html/index.html', to: __dirname+'/dist/index.html' },
-			{ from: './src/css/exam-paper.css', to: __dirname+'/dist/css/exam-paper.css' },
+			{ from: './src/html/datakom.html',                 to: __dirname+'/dist/datakom.html' },
+			{ from: './src/html/linux.html',                   to: __dirname+'/dist/linux.html' },
+			{ from: './src/css/exam-paper.css',                to: __dirname+'/dist/css/exam-paper.css' },
+			{ from: './src/css/exam-paper-linux.css',          to: __dirname+'/dist/css/exam-paper-linux.css' },
 			{ from: './node_modules/katex/dist/katex.min.css', to: __dirname+'/dist/css/katex.min.css' },
-			{ from: './node_modules/katex/dist/fonts', to: __dirname+'/dist/css/fonts' }
+			{ from: './node_modules/katex/dist/fonts',         to: __dirname+'/dist/css/fonts' }
 		])
 	]
 }
